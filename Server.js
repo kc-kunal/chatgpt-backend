663d6d205ddaa5ac8540de73eb106de1192823fc
kc-Kunal <kc9998008@gmail.com>
2025-08-03 01:57:31 +0530

    Use dynamic port for Render deployment

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ import fetch from "node-fetch";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
